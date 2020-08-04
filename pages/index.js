432ba128d7a4551f65a03d233d11e47b9b450911
maku693 <maku693@hatena.ne.jp>
2020-08-04 21:44:51 +0900

    getStaticProps

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,12 @@ const hello =
   getConfig().serverRuntimeConfig.hello ||
   getConfig().publicRuntimeConfig.hello;
 
-export default function Home() {
-  return (<>{hello}</>)
+export default function Home({ staticHello }) {
+  return (<>hello: {hello}<br/>staticHello: {staticHello}</>)
+}
+
+export function getStaticProps() {
+  return {
+    props: { staticHello: hello }
+  }
 }
